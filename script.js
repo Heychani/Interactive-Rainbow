432cb70a9832b168bb1081a709a253a21b46b8d0
Chanique Lombard <98527335+Heychani@users.noreply.github.com>
2022-08-28 16:14:37 +0200

    Such a fun challenge DONE

--- script.js
+++ script.js
@@ -1,34 +1,27 @@
 function redColour() {
   document.getElementById("red").style.backgroundColor = "red";
-  document.getElementById("red").innerHTML = "red";
 }
 
 function orangeColour() {
   document.getElementById("orange").style.backgroundColor = "orange";
-  document.getElementById("orange").innerHTML = "orange";
 }
 
 function yellowColour() {
   document.getElementById("yellow").style.backgroundColor = "yellow";
-  document.getElementById("yellow").innerHTML = "yellow";
 }
 
 function greenColour() {
   document.getElementById("green").style.backgroundColor = "green";
-  document.getElementById("green").innerHTML = "green";
 }
 
 function blueColour() {
   document.getElementById("blue").style.backgroundColor = "blue";
-  document.getElementById("blue").innerHTML = "blue";
 }
 
 function purpleColour() {
   document.getElementById("purple").style.backgroundColor = "purple";
-  document.getElementById("purple").innerHTML = "purple";
 }
 
 function violetColour() {
   document.getElementById("violet").style.backgroundColor = "violet";
-  document.getElementById("violet").innerHTML = "violet";
 }
